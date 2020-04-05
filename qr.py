8acd58b486475abb2c353ce0f37b83d8ab9e8308
Marcel <marcel.roux.email@gmail.com>
2020-04-05 21:10:46 +0200

    Added ability to add logo to QR code.

--- qr.py
+++ qr.py
@@ -1,5 +1,5 @@
 import qrcode
-from qrcode.constants import ERROR_CORRECT_Q
+from qrcode.constants import ERROR_CORRECT_L, ERROR_CORRECT_Q
 
 class QR:
 
@@ -12,11 +12,35 @@ class QR:
             border=border
         )
 
-        self.code.add_data(data)
+        self.data = data
     
-    def make(self, fit=True):
+    def make(self, fit=True, fill_color='black', back_color='white'):
+        self.fit = fit
+        self.fill_color = fill_color
+        self.back_color = back_color
+
+        self.code.clear()
+        self.code.add_data(self.data)
         self.code.make(fit=fit)
-        self.img = self.code.make_image(fill_color='black', back_color='white')
+        self.img = self.code.make_image(fill_color=fill_color, back_color=back_color)
+    
+    def add_logo(self, logo):
+        logo = logo.convert('RGBA')
+
+        # Adding logo reduces surface area of code.
+        # To compensate, increase QR version.
+        self.code.version += 3
+        self.make(self.fit, self.fill_color, self.back_color)
+
+        self.img = self.img.convert('RGBA')
+
+        logo_w, logo_h = logo.size
+        img_w, img_h = self.img.size
+
+        offset_h = (img_h - logo_h) // 2
+        offset_w = (img_w - logo_w) // 2
+
+        self.img.paste(logo, (offset_w, offset_h), mask=logo)
 
     def save(self, filename):
         self.img.save(f'./out/{filename}')
