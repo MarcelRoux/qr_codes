7f421ee7891d3b77067af6d82ffd97e89453aeb9
Marcel <marcel.roux.email@gmail.com>
2020-04-05 21:11:41 +0200

    Updated reference to logo image.

--- logo.py
+++ logo.py
@@ -4,35 +4,37 @@ class LogoRound:
 
     def __init__(self, logo_filename, offset=4):
         # Open logo file.
-        self.logo = Image.open(logo_filename).convert('RGBA')
+        self.img = Image.open(logo_filename).convert('RGBA')
         self.offset = offset
     
     def add_logo_background(self, background_fill='white'):
         # Get logo size.
-        logo_w, logo_h = self.logo.size
-        radius = logo_w // 2
+        img_w, img_h = self.img.size
+        radius = img_w // 2
 
         # Create transparent background.
-        background = Image.new('RGBA', (logo_w, logo_h), (255, 255, 255, 0))
+        background = Image.new('RGBA', (img_w, img_h), (255, 255, 255, 0))
 
         # Calculate bounding box and reduce diameter of circle by offset.
         top_left = (0 + self.offset,  + self.offset)
-        bottom_right = (logo_w - self.offset, logo_h - self.offset)
+        bottom_right = (img_w - self.offset, img_h - self.offset)
         bounding_box = [top_left, bottom_right]
 
         # Add non-transparent circle to background.
         ImageDraw.Draw(background).ellipse(bounding_box, fill=background_fill)
 
         # Overlay logo on background.
-        background.paste(self.logo, (0, 0), mask=self.logo)
+        background.paste(self.img, (0, 0), mask=self.img)
 
-        self.logo = background
+        self.img = background
+
+        self.img = background
     
     def get_logo_size(self):
-        return self.logo.size
+        return self.img.size
     
     def save(self, filename):
-        self.logo.save(f'./icons/{filename}')
+        self.img.save(f'./icons/{filename}')
 
 
 
