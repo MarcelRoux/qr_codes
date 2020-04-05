feda8a38b5eaf73f8b0c2d3c5c3df230bc03e3db
Marcel <marcel.roux.email@gmail.com>
2020-04-05 21:35:46 +0200

    Added box size option and linkedin data.

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ resources = {
         'url': 'https://github.com/MarcelRoux',
         'colour': '#171516',
         'add_logo_background': True,
-        'logo_background_fill': 'white'
+        'logo_background_fill': 'white',
+        'box_size': 8
+    },
+    'linked_in': {
+        'qr_filename': 'linkedin.png',
+        'logo_filename': 'linkedin.png',
+        'logo': './icons/linkedin_logo_512.png',
+        'url': 'https://www.linkedin.com/in/rouxmarcel/',
+        'colour': '#0077B5',
+        # 'add_logo_background': True,
+        # 'logo_background_fill': 'white',
+        'box_size': 32
     }
 }
 
@@ -18,12 +29,12 @@ def main():
     for name, resource in resources.items():
         # Logo section.
         logo = LogoRound(resource['logo'])
-        if('add_logo_background' in resource):
+        if('add_logo_background' in resource and resource['add_logo_background'] == True):
             logo.add_logo_background(resource['logo_background_fill'])
         logo.save(resource['logo_filename'])
 
         # QR section.
-        qr = QR(resource['url'], box_size=8, border=2,)
+        qr = QR(resource['url'], box_size=resource['box_size'], border=2)
         qr.make(fit=True, fill_color=resource['colour'])
         qr.add_logo(logo.img)
         qr.save(resource['qr_filename'])
