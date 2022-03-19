21045de4494f2f8b98408c37158cb5853fbbfd0d
Marcel <marcel.roux.email@gmail.com>
2022-03-19 11:13:39 -0400

    Added printing to narrate process.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ with open('./data.json', 'r') as f:
 def main():
 
     for name, resource in resources.items():
+        print(f'Creating logo for {name}')
         # Logo section.
         logo = LogoRound(resource.get('logo', 'missing.png'),
                          offset=resource.get('offset', 4))
@@ -18,13 +19,16 @@ def main():
                                                   'white'))
         logo.save(resource.get('logo_filename', 'logo_out.png'))
 
+        print(f'Creating QR code for {name}')
         # QR section.
         qr = QR(resource.get('url', 'https//:example.com'),
                 box_size=resource.get('box_size', 8),
                 border=2)
         qr.make(fit=True, fill_color=resource.get('colour', 'white'))
         qr.add_logo(logo.img)
-        qr.save(resource.get('qr_filename', 'qr_out.png'))
+        output = qr.save(resource.get('qr_filename', 'qr_out.png'))
+
+        print(f'Created logo-embedded QR code for {name} at {output}')
 
 
 if __name__ == "__main__":
