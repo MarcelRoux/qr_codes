5b23817271a6f78214c2a6c5e08d02bc627be769
Marcel <marcel.roux.email@gmail.com>
2022-03-19 11:11:26 -0400

    Implemented .get for getting dict values by key.

--- main.py
+++ main.py
@@ -11,16 +11,21 @@ def main():
 
     for name, resource in resources.items():
         # Logo section.
-        logo = LogoRound(resource['logo'])
-        if('add_logo_background' in resource and resource['add_logo_background'] == True):
-            logo.add_logo_background(resource['logo_background_fill'])
-        logo.save(resource['logo_filename'])
+        logo = LogoRound(resource.get('logo', 'missing.png'),
+                         offset=resource.get('offset', 4))
+        if(resource.get('add_logo_background', False)):
+            logo.add_logo_background(resource.get('logo_background_fill',
+                                                  'white'))
+        logo.save(resource.get('logo_filename', 'logo_out.png'))
 
         # QR section.
-        qr = QR(resource['url'], box_size=resource['box_size'], border=2)
-        qr.make(fit=True, fill_color=resource['colour'])
+        qr = QR(resource.get('url', 'https//:example.com'),
+                box_size=resource.get('box_size', 8),
+                border=2)
+        qr.make(fit=True, fill_color=resource.get('colour', 'white'))
         qr.add_logo(logo.img)
-        qr.save(resource['qr_filename'])
+        qr.save(resource.get('qr_filename', 'qr_out.png'))
+
 
 if __name__ == "__main__":
     main()
